684a5b767fe8efdfd64ec46b349361434538c9a3
mamapig <mamapig@users.noreply.github.com>
2020-02-05 23:23:02 -0300

    practicas con animaciones

---
+++ Little-Final/Assets/TEST/PlayerMovement/TestMovementPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovementPlayer : MonoBehaviour
+{
+	public CapsuleCollider landCollider;
+	public TestJump animatorScript;
+	private void Awake()
+	{
+		animatorScript.landCollider = landCollider;
+	}
+}
