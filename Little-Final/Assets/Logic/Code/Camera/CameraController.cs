2b591e27eb57fd458833be5f4c3988d8d89c4242
Juan Pablo Varela <jvarelaaloisio@users.noreply.github.com>
2020-12-11 00:58:04 -0300

    finished camera transitions

---
+++ Little-Final/Assets/Logic/Code/Camera/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UpdateManagement;
+public class CameraController : MonoBehaviour, IUpdateable
+{
+	public Animator animator;
+	void Start()
+	{
+		UpdateManager.Instance.Subscribe(this);
+	}
+
+	public void OnUpdate()
+	{
+		animator.SetBool("Input X", Mathf.Abs(Input.GetAxis("Camera X")) > 0);
+	}
+}
