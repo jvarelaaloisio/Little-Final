f9b04a10e0d8940b990568e783f52a3a8713637a
FacundoLv <FacundoLV@live.com.ar>
2022-04-01 03:03:29 -0300

    Async scene loading menu added

---
+++ Little-Final/Assets/Scripts/Menues/ScenesDataContainer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+public static class ScenesDataContainer
+{
+    public static int TitleScreen => 1;
+
+    public static readonly List<int> LevelOneScenes = new List<int>
+    {
+        2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+    };
+}
