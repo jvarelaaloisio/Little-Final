e36779108e5248d0f4ea9054365009d2bcbe81b8
Juan Pablo Varela <33493616+jvarelaaloisio@users.noreply.github.com>
2022-04-28 14:20:15 -0300

    Changes in player and camera

---
+++ Little-Final/Assets/Scripts/CinemachineControl/CopyCameraState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Cinemachine;
+using UnityEngine;
+
+namespace CinemachineControl
+{
+	public class CopyCameraState : MonoBehaviour
+	{
+		[SerializeField]
+		private Transform camera;
+
+		[SerializeField]
+		private CinemachineFreeLook freeLook;
+
+		[ContextMenu("Copy Virtual Cam position To FreeLook")]
+		private void CopyVirtualToFreeLook()
+		{
+			freeLook.ForceCameraPosition(camera.position, camera.rotation);
+		}
+
+		public void ResetFreeLookCamera(ICinemachineCamera toCam, ICinemachineCamera fromCam)
+		{
+			if (ReferenceEquals(toCam, freeLook))
+			{
+				CopyVirtualToFreeLook();
+			}
+		}
+	}
+}
