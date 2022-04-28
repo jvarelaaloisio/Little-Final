e365617d14263b84bcba91d975bf4cfd35813e6d
Juan Pablo Varela <33493616+jvarelaaloisio@users.noreply.github.com>
2022-04-28 14:36:40 -0300

    Push Ability

--- Little-Final/Assets/Scripts/Player/Abilities/Push.cs
+++ Little-Final/Assets/Scripts/Player/Abilities/Push.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using Player.PlayerInput;
 
@@ -7,9 +8,11 @@ namespace Player.Abilities
 	[CreateAssetMenu(menuName = "Abilities/Push", fileName = "Ability_Push")]
 	public class Push : Ability
 	{
-		[SerializeField] private Vector3 direction;
-
-		private ForceRequest _forceRequest;
+		[SerializeField]
+		private Vector3 direction;
+		
+		[SerializeField]
+		private AnimationCurve forceDecay = AnimationCurve.Linear(0, 1, 1, 0);
 
 		protected override bool ValidateInternal(PlayerController controller)
 		{
@@ -18,8 +21,28 @@ namespace Player.Abilities
 
 		public override void Use(PlayerController controller)
 		{
-			Debug.Log("push");
-			controller.Body.RequestForce(new ForceRequest(controller.transform.TransformDirection(direction), ForceMode.Impulse));
+			// controller.Body.RequestForce(new ForceRequest(controller.transform.TransformDirection(direction),
+			// 											ForceMode.Impulse));
+			float duration = forceDecay.keys[forceDecay.keys.Length - 1].time - forceDecay.keys[0].time;
+			controller.StartCoroutine(AddForceOverTime(controller, direction, duration, forceDecay.Evaluate));
+		}
+
+		private IEnumerator AddForceOverTime(PlayerController controller,
+											Vector3 direction,
+											float duration,
+											Func<float, float> evlauate)
+		{
+			Vector3 normalDirection = direction.normalized;
+			float force = direction.magnitude;
+			float start = Time.time;
+			float now;
+			while ((now = Time.time - start) <= duration)
+			{
+				float currentForce = force * evlauate(now);
+				Vector3 scaledDir = controller.transform.TransformDirection(normalDirection) * currentForce * Time.deltaTime;
+				controller.Body.RequestForce(new ForceRequest(scaledDir, ForceMode.Force));
+				yield return null;
+			}
 		}
 	}
 }
