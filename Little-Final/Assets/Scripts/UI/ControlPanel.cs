973d2a4e891518cf959dccb8e77b15bcf9984e91
FacundoLv <FacundoLV@live.com.ar>
2022-06-08 22:12:07 -0300

    Scene switcher buttons added

---
+++ Little-Final/Assets/Scripts/UI/ControlPanel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class ControlPanel : MonoBehaviour
+    {
+        [SerializeField] private GameObject container;
+
+        private bool _isOn = false;
+
+        private void Awake()
+        {
+            container.SetActive(_isOn);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape)) Toggle();
+        }
+
+        private void Toggle()
+        {
+            _isOn = !_isOn;
+            container.SetActive(_isOn);
+        }
+    }
+}
