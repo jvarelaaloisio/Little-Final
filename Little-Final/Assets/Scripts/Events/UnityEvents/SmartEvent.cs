fac165aa12b60cfb576a0b3da842aeac32d3cc3e
Juan Pablo Varela <33493616+jvarelaaloisio@users.noreply.github.com>
2022-06-25 18:11:14 -0300

    Added smart events

---
+++ Little-Final/Assets/Scripts/Events/UnityEvents/SmartEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Events.UnityEvents
+{
+	[System.Serializable]
+	public class SmartEvent : UnityEvent
+	{
+		private event Action OnEventInternal;
+
+		public SmartEvent()
+		{
+			OnEventInternal = RaiseEvent;
+		}
+
+		[ContextMenu("Raise Event")]
+		private void RaiseEvent()
+		{
+			base.Invoke();
+		}
+		
+		public static implicit operator Action(SmartEvent original) => original.OnEventInternal;
+		public static SmartEvent operator +(SmartEvent original, Action action)
+		{
+			original.AddListener(action.Invoke);
+			return original;
+		}
+	}
+}
---
+++ Little-Final/Assets/Scripts/Events/UnityEvents/SmartEventTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Events.UnityEvents
+{
+	public class SmartEventTest : MonoBehaviour
+	{
+		[SerializeField]
+		private SmartEvent mySmartEvent;
+		[ContextMenu("Invoke From UnityEvent")]
+		private void InvokeFromUnityEvent()
+		{
+			mySmartEvent.Invoke();
+			((Action)mySmartEvent)();
+		}
+		[ContextMenu("Invoke From Action")]
+		private void InvokeFromAction()
+		{
+			mySmartEvent += AdditionalBehaviour;
+			Action action = mySmartEvent;
+			action();
+		}
+
+		private void AdditionalBehaviour()
+			=> Debug.Log("This Action was added after");
+	}
+}
