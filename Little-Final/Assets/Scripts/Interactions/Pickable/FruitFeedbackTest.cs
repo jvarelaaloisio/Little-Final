02602cbe7a016e154e56a7739de21b02c6c7425c
Juan Pablo Varela <33493616+jvarelaaloisio@users.noreply.github.com>
2022-06-29 20:01:21 -0300

    Strawberry now shows bites instead of changing scale

--- Little-Final/Assets/Scripts/Interactions/Pickable/FruitFeedbackTest.cs
+++ Little-Final/Assets/Scripts/Interactions/Pickable/FruitFeedbackTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Interactions.Pickable
@@ -9,17 +10,34 @@ namespace Interactions.Pickable
 		private float[] scales = new float[5];
 
 		[SerializeField]
+		private FruitMeshData[] fruitMeshData;
+		
+		[SerializeField]
 		private float scaleDuration;
 
 		[SerializeField]
 		private AnimationCurve scaling = AnimationCurve.EaseInOut(0, 0, 1, 1);
+
+		[SerializeField]
+		private MeshFilter meshFilter;
+
+		[SerializeField]
+		private MeshRenderer meshRenderer;
+
+		private void OnValidate()
+		{
+			if (!meshFilter) TryGetComponent(out meshFilter);
+			if (!meshRenderer) TryGetComponent(out meshRenderer);
+		}
 
 		public void UpdateScale(int pos)
 		{
 			Keyframe[] scalingKeys = scaling.keys;
 			float duration = scalingKeys[scalingKeys.Length - 1].time - scalingKeys[0].time;
 			pos = Mathf.Clamp(0, pos, scales.Length - 1);
-			StartCoroutine(TransitionScale(transform.localScale, Vector3.one * scales[pos], duration));
+			meshFilter.mesh = fruitMeshData[pos].Mesh;
+			meshRenderer.sharedMaterial = fruitMeshData[pos].Material;
+			// StartCoroutine(TransitionScale(transform.localScale, Vector3.one * scales[pos], duration));
 		}
 
 		private IEnumerator TransitionScale(Vector3 from, Vector3 to, float duration)
@@ -33,6 +51,19 @@ namespace Interactions.Pickable
 				transform.localScale = Vector3.Lerp(from, to, smoothLerp);
 				yield return null;
 			}
+		}
+		[Serializable]
+		private struct FruitMeshData
+		{
+			[SerializeField]
+			private Mesh mesh;
+
+			[SerializeField]
+			private Material material;
+			
+			public Mesh Mesh => mesh;
+
+			public Material Material => material;
 		}
 	}
 }
