3a723ee819810f1552c1afa8f5c1b5978fca725c
Juan Pablo Varela <33493616+jvarelaaloisio@users.noreply.github.com>
2022-07-06 23:55:16 -0300

    Added whitebox view for level 1

---
+++ Little-Final/Assets/Scripts/Extras/LayerEnablerOnCamera.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Core.Debugging;
+using UnityEngine;
+
+namespace Extras
+{
+	public class LayerEnablerOnCamera : MonoBehaviour
+	{
+		[SerializeField]
+		private DebuggerSafeContainer debugger;
+
+		[SerializeField]
+		private LayerMask cullingMaskOverride;
+		[Serializable]
+		private struct DebuggerSafeContainer
+		{
+			[SerializeField]
+			private Debugger debugger;
+
+			public Debugger Value
+			{
+				get
+				{
+					if (!debugger)
+						Debug.LogError("Debugger is null");
+
+					return debugger;
+				}
+			}
+		}
+		
+		private void Start()
+		{
+			Camera.main.cullingMask = cullingMaskOverride;
+			debugger.Value.Log("Debugs", "camera culling mask overwritten", this);
+		}
+	}
+}
