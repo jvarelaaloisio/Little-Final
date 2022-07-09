46bb88ac3fb04b853cb39c97ffc11a5c587eddfc
Juan Pablo Varela <33493616+jvarelaaloisio@users.noreply.github.com>
2022-07-09 19:45:51 -0300

    hide and lock cursor option added when pressing F2 in play mode

---
+++ Little-Final/Assets/Scripts/Extras/HideAndLockCursor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Extras
+{
+	public class HideAndLockCursor : MonoBehaviour
+	{
+		private void Awake()
+		{
+			Cursor.lockState = CursorLockMode.Confined;
+		}
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.F2)) Cursor.visible = !Cursor.visible;
+		}
+	}
+}
