13ab4daadd648092203e943e69542bef969a31a3
Juan Pablo Varela <115322291+jvarela-ttg@users.noreply.github.com>
2023-01-25 20:04:53 -0300

    diverse changes

---
+++ Little-Final/Assets/Scripts/Scenes/Level.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEngine.SceneManagement;
+#endif
+
+namespace Scenes
+{
+	[Serializable]
+	public struct Chunk
+	{
+		[SerializeField]
+		public int[] sceneIndexes;
+		
+		public int activeScene;
+	}
+
+	[CreateAssetMenu(fileName = "Level", menuName = "Levels/Level", order = 0)]
+	public class Level : ScriptableObject
+	{
+		[SerializeField]
+		private Chunk[] chunks;
+
+#if UNITY_EDITOR
+		private void OnValidate()
+		{
+			foreach (Chunk chunk in chunks)
+			{
+				foreach (var sceneIndex in chunk.sceneIndexes)
+				{
+					if (!SceneManager.GetSceneByBuildIndex(sceneIndex).IsValid())
+						Debug.LogError($"index {sceneIndex} is not in build");
+				}
+			}
+		}
+#endif
+	}
+}
