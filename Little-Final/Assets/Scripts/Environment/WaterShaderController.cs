3ed6d5dafe266a72634d46293639dfa1d4dc6fb5
Yoisus <jesus.a.vargas.calvi@gmail.com>
2023-02-02 09:55:01 -0300

    Drimys Packages Prefabs Revisar Scripts .Gitatributes se renuve el filtro de PSD

---
+++ Little-Final/Assets/Scripts/Environment/WaterShaderController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Environment
+{
+    [ExecuteInEditMode]
+    public class WaterShaderController : MonoBehaviour
+    {
+        //Properties
+        public Transform clearPoint_1;
+        public Transform clearPoint_2;
+        
+        //Shader Properties IDs
+        private static readonly int ClearPoint_1_ID = Shader.PropertyToID("G_Water_ClearZone_1");
+        private static readonly int ClearPoint_2_ID = Shader.PropertyToID("G_Water_ClearZone_2");
+
+        private void Update()
+        {
+            if(clearPoint_1)
+                Shader.SetGlobalVector(ClearPoint_1_ID,clearPoint_1.position);
+            if(clearPoint_2)
+                Shader.SetGlobalVector(ClearPoint_2_ID,clearPoint_2.position);
+        }
+    }
+}
