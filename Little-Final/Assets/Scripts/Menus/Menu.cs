824ab35e5b0253d0dc8008888e6e7f69f1faab64
jvarelaaloisio <juampyvarela@gmail.com>
2024-02-23 18:13:38 -0300

    Fixed lighting bug in scene loading Added menu feature to select again after deselecting in menu

--- Little-Final/Assets/Scripts/Menus/Menu.cs
+++ Little-Final/Assets/Scripts/Menus/Menu.cs
@@ -7,11 +7,36 @@ namespace Menus
     public class Menu : MonoBehaviour
     {
         [SerializeField] private GameObject defaultSelection;
+        [SerializeField] private string horizontalAxis  = "Horizontal";
+        [SerializeField] private string verticalAxis = "Vertical";
+        private GameObject _lastSelection;
+        private EventSystem _eventSystem;
 
         protected virtual void OnEnable()
         {
-            if (defaultSelection && EventSystem.current)
-                EventSystem.current.SetSelectedGameObject(defaultSelection);
+            _eventSystem = EventSystem.current;
+            if (!_eventSystem)
+            {
+                enabled = false;
+                return;
+            }
+            if (defaultSelection)
+                _eventSystem.SetSelectedGameObject(defaultSelection);
+        }
+
+        protected virtual void Update()
+        {
+            var currentSelection = _eventSystem.currentSelectedGameObject;
+            if (!currentSelection
+                && (Mathf.Abs(Input.GetAxis(horizontalAxis)) > 0
+                    || Mathf.Abs(Input.GetAxis(verticalAxis)) > 0))
+            {
+                _eventSystem.SetSelectedGameObject(_lastSelection);
+            }
+            if (currentSelection != null && currentSelection != _lastSelection)
+            {
+                _lastSelection = currentSelection;
+            }
         }
     }
 }
