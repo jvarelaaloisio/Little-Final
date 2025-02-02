a774ade5f32c5b45ccb0b719868a1955c488bab0
jvarelaaloisio <juampyvarela@gmail.com>
2025-02-02 15:34:02 -0300

    Added: - Node logic for FsmEditor.cs

--- Little-Final/Assets/Editor/UIToolkit/FsmEditor.cs
+++ Little-Final/Assets/Editor/UIToolkit/FsmEditor.cs
@@ -2,37 +2,43 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class FsmEditor : EditorWindow
+namespace Editor.UIToolkit
 {
-    [SerializeField]
-    private VisualTreeAsset m_VisualTreeAsset = default;
-    [SerializeField]
-    private VisualTreeAsset node = default;
-
-    [MenuItem("Window/UI Toolkit/FsmEditor")]
-    public static void ShowExample()
+    public class FsmEditor : EditorWindow
     {
-        FsmEditor wnd = GetWindow<FsmEditor>();
-        wnd.titleContent = new GUIContent("FsmEditor");
-    }
+        [SerializeField]
+        private VisualTreeAsset m_VisualTreeAsset = default;
+        [SerializeField]
+        private VisualTreeAsset node = default;
 
-    public void CreateGUI()
-    {
-        // Each editor window contains a root VisualElement object
-        VisualElement root = rootVisualElement;
+        private Node _node;
 
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-        VisualElement label = new Label("Hello World! From C#");
-        root.Add(label);
-
-        // Instantiate UXML
-        VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
-        root.Add(labelFromUXML);
-        if (node)
+        [MenuItem("Window/UI Toolkit/FsmEditor")]
+        public static void ShowExample()
         {
-            var nodeChild = node.Instantiate();
-            nodeChild.style.position = new StyleEnum<UnityEngine.UIElements.Position>(UnityEngine.UIElements.Position.Absolute);
-            root.Add(nodeChild);
+            FsmEditor wnd = GetWindow<FsmEditor>();
+            wnd.titleContent = new GUIContent("FsmEditor");
+        }
+
+        public void CreateGUI()
+        {
+            // Each editor window contains a root VisualElement object
+            VisualElement root = rootVisualElement;
+
+            // VisualElements objects can contain other VisualElement following a tree hierarchy.
+            VisualElement label = new Label("Hello World! From C#");
+            root.Add(label);
+
+            // Instantiate UXML
+            VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
+            root.Add(labelFromUXML);
+            if (node)
+            {
+                var nodeChild = node.Instantiate();
+                nodeChild.style.position = new StyleEnum<UnityEngine.UIElements.Position>(UnityEngine.UIElements.Position.Absolute);
+                _node = new Node(nodeChild);
+                root.Add(nodeChild);
+            }
         }
     }
 }
