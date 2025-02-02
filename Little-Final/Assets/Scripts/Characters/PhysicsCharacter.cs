5303a499d5b52fe3700a2d1e1d338be2a60ca7ed
jvarelaaloisio <juampyvarela@gmail.com>
2025-02-02 15:34:17 -0300

    Refactor: - Moved FallingController to it's own file

--- Little-Final/Assets/Scripts/Characters/PhysicsCharacter.cs
+++ Little-Final/Assets/Scripts/Characters/PhysicsCharacter.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using Core.Attributes;
 using Core.Extensions;
@@ -7,43 +6,6 @@ using UnityEngine;
 
 namespace Characters
 {
-    public class FallingController
-    {
-        private readonly Rigidbody _rigidbody;
-
-        public FallingController(Rigidbody rigidbody)
-            => _rigidbody = rigidbody;
-
-        /// <summary>
-        /// Event risen when <see cref="_rigidbody"/>.<see cref="Rigidbody.velocity"/>.y becomes less than 0
-        /// </summary>
-        public event Action OnStartFalling = delegate { };
-
-        /// <summary>
-        /// Event risen when <see cref="Velocity"/>.y becomes less than 0
-        /// </summary>
-        public event Action OnStopFalling = delegate { };
-
-        public bool IsFalling { get; private set; }
-
-        /// <summary>
-        /// Call this method to check if any events should be risen
-        /// </summary>
-        public void Update()
-        {
-            switch (IsFalling)
-            {
-                case true when _rigidbody.velocity.y > -0.1f:
-                    IsFalling = false;
-                    OnStopFalling();
-                    break;
-                case false when _rigidbody.velocity.y < -0.1f:
-                    IsFalling = true;
-                    OnStartFalling();
-                    break;
-            }
-        }
-    }
     [RequireComponent(typeof(Rigidbody))]
     public class PhysicsCharacter : Character<PhysicsCharacter.Data>
     {
