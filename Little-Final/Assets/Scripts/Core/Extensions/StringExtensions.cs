911d4c46ecd3ad5d305795dfb85923f7f91e45b1
jvarelaaloisio <juampyvarela@gmail.com>
2025-03-03 04:16:24 -0300

    Refactor: Core.Debugging.StringFormatting => Core.Extensions.StringExtensions

--- Little-Final/Assets/Scripts/Core/Extensions/StringExtensions.cs
+++ Little-Final/Assets/Scripts/Core/Extensions/StringExtensions.cs
@@ -1,6 +1,6 @@
-﻿namespace Core.Debugging
+﻿namespace Core.Extensions
 {
-	public static class StringFormatting
+	public static class StringExtensions
 	{
 		public static string Colored(this object message, string color) => $"<color={color}>{message}</color>";
 		public static string Bold(this object message) => $"<b>{message}</b>";
