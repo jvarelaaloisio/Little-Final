1cfd5e524469ab79a9cf5672fd488796a4737381
jvarelaaloisio <juampyvarela@gmail.com>
2025-05-24 18:46:33 -0300

    Added Populate provider for Camera

---
+++ Little-Final/Assets/Scripts/DataProviders/PopulateCameraProvider.cs
@@ -0,0 +1,8 @@
+using Core.Providers;
+using UnityEngine;
+
+namespace DataProviders
+{
+	[AddComponentMenu("Data/Populate Camera Provider")]
+	public class PopulateCameraProvider : PopulateComponentProvider<Camera> { }
+}
